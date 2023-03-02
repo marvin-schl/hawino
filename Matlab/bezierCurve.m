ef20c07b4b2e53a8fbc5e9089d3ec494d6c6c02f
Tom <tom.westerling@haw-hamburg.de>
2023-03-02 14:26:21 +0100

    Cleaned up

--- Matlab/bezierCurve.m
+++ Matlab/bezierCurve.m
@@ -1,4 +1,5 @@
 classdef BezierCurve < Spline
+    % P U B L I C  P R O P E R T I E S ====================================
     properties
         startPoint          % Entry point of the Bezier curve [x, y]
         endPoint            % Exit point of the Bezier curve [x, y]
@@ -7,159 +8,141 @@ classdef BezierCurve < Spline
         curvePoints         % array of curve points [x1, y1; ...; xn, yn]
     end
 
+    % P R I V A T E  P R O P E R T I E S ==================================
     properties (Access = private)
         controlPoint        % Control point of the Bezier curve [x, y]
     end
 
-    methods 
+    % P U B L I C  M E T H O D S ==========================================
+    methods
+        % -----------------------------------------------------------------
+        % FUNCTION: BezierCurve(x, y, r, dx)
+        % Constructor
+        % Inputs:   [x]     x-coordinates of previous WP, control point, next WP
+        %           [y]     y-coordinates of previous WP, control point, next WP
+        %           r       radius of Bezier circle with respect to control
+        %           dx      sampling distance
+        % Outputs:  none
         function obj = BezierCurve(x, y, r, dx)
-            
+            % Initializations
             prevWP = struct("x", x(1), "y", y(1));
-            nextWP = struct("x", x(3), "y", y(3));      
+            nextWP = struct("x", x(3), "y", y(3));
             obj.controlPoint = struct("x", x(2), "y", y(2));
-    
             obj.r = r;
             obj.dx = dx;
             obj.startPoint = struct("x", 0, "y", 0);
             obj.endPoint = struct("x", 0, "y", 0);
 
+            % 1.) Calculate start- and end point of curve (intersections of
+            % linear waypoint connections with circle around control point)
             [obj.startPoint, obj.endPoint] = obj.getStartEndPoint(prevWP, nextWP);
-            [xCurve,yCurve] = obj.getPoint([0:dx:1]);
-            obj.curvePoints = struct("x", xCurve, "y", yCurve);
+
+            % 2.) Calculate all points on curve and save as struct
+            % (all points on parameter interval s = 0 : dx : 1)
+            [curvePointsX, curvePointsY] = obj.getPoint([0 : dx : 1]);
+            obj.curvePoints = struct("x", curvePointsX, "y", curvePointsY);
+
+            % 3.) Calculate length of curve
             obj.length = obj.calculateLength();
-           
         end
-        
-        function curvePoints = getCurvePoints(obj, startPoint, controlPoint, endPoint, dx)
-%             n = 3;
-%             n1 = n - 1;
-%             
-%             for i = 0 : 1 : n1
-%                 sigma(i+1) = factorial(n1) / (factorial(i)*factorial(n1-i)); 
-%             end
-%             
-%             l = [];
-%             UB = [];
-%             
-%             for u = 0 : dx : 1
-%                 for d = 1 : n
-%                     UB(d) = sigma(d)*((1-u)^(n-d))*(u^(d-1));
-%                 end
-%                 
-%                 l = cat(1,l,UB);                                       
-%             end
-%             
-%             curvePoints = l * [startPoint.x, startPoint.y; controlPoint.x, controlPoint.y; endPoint.x, endPoint.y];
-            
-        end
+        % -----------------------------------------------------------------
 
+        % -----------------------------------------------------------------
+        % FUNCTION: getPoint(s)
+        % Returns either a corresponding point for a given scalar parameter
+        % or an array of points for a given parameter vector. Parameter s
+        % can be normalized (s = [0 : dx : 1]) or absolute (s = [0 : dx :
+        % length(curve)]
+        % Inputs:   [s]     parameter: scalar or vector, normalized or absolute
+        % Outputs:  [x, y]  corresponding point (array of points) for parameter (vector)
         function [x,y] = getPoint(obj, s)
-            n = 3;
-            n1 = n - 1;
-            
-            for i = 0 : 1 : n1
-                sigma(i+1) = factorial(n1) / (factorial(i)*factorial(n1-i)); 
+            % normalize parameter if required
+            if s(length(s)) ~= 1
+                t = s/obj.length;
+            else
+                t = s;
             end
             
-            l = [];
-            UB = [];
-            
-             %s = s / obj.length;
-             if max(s) ~= 1
-                   s = s/obj.length;
-             end
-
-            %if any((s < 0)) || any((s > 1))
-               % throw(MException("BezierCurve:NotDefined","Invalid parameter range! Expected s in range [0 ... obj.length]"))
-            %else
-            if true
-                for i = 1:length(s)
-                    u = s(i);
-                    for d = 1 : n
-                        UB(d) = sigma(d) * ((1 - u)^(n - d))*(u^(d - 1));
-                    end
-
-                    l = cat(1, l, UB);                                       
-                end
-                point = l * [obj.startPoint.x, obj.startPoint.y; obj.controlPoint.x, obj.controlPoint.y; obj.endPoint.x, obj.endPoint.y];
-                x = point(:,1);
-                y = point(:,2);
-            end
-%            t = s;
-%            if max(s) ~= 1
-%                t = s/obj.length;
-%            end
-% 
-%            x = (obj.startPoint.x - 2*obj.controlPoint.x + obj.endPoint.x)*t.^2 + (2*obj.controlPoint.x - 2*obj.startPoint.x) * t + obj.startPoint.x;
-%            y = (obj.startPoint.y - 2*obj.controlPoint.y + obj.endPoint.y)*t.^2 + (2*obj.controlPoint.y - 2*obj.startPoint.y) * t + obj.startPoint.y;
+            % calculate corresponding point (hard-coded for 3rd order Bezier curve)
+            x = (obj.startPoint.x - 2*obj.controlPoint.x + obj.endPoint.x)*t.^2 + (2*obj.controlPoint.x - 2*obj.startPoint.x) * t + obj.startPoint.x;
+            y = (obj.startPoint.y - 2*obj.controlPoint.y + obj.endPoint.y)*t.^2 + (2*obj.controlPoint.y - 2*obj.startPoint.y) * t + obj.startPoint.y;
         end
-
+        % -----------------------------------------------------------------
     end
-     
-    methods (Access = private) 
- 
+
+    % P R I V A T E  M E T H O D S ========================================
+    methods (Access = private)
+        % -----------------------------------------------------------------
+        % FUNCTION: getStartEndPoint(prevWP, nextWP)
+        % Returns corresponding start- and end points for a given previous
+        % and following waypoint. The middle waypoint is the control point
+        % of the curve. The start- and end points are the intersections of
+        % the linear waypoint connections and a circle with radius r around
+        % the control point. Start- and end points are the connection 
+        % points of linear and Bezier splines.
+        % Inputs:
         function [startPoint, endPoint] = getStartEndPoint(obj, prevWP, nextWP)
             % Start point:
             % - angle of incidence of former linear part
             phi1 = atan2(prevWP.y - obj.controlPoint.y, prevWP.x - obj.controlPoint.x);
-            
+
             % - x-coordinates where former linear part intersects with circle
             zpx11 = obj.controlPoint.x - obj.r * cos(phi1);
             zpx12 = obj.controlPoint.x + obj.r * cos(phi1);
             % - y-coordinates where former linear part intersects with circle
             zpy11 = obj.controlPoint.y - obj.r * sin(phi1);
             zpy12 = obj.controlPoint.y + obj.r * sin(phi1);
-        
-            % - decide which of the 2 solutions the relevant one 
+
+            % - decide which of the 2 solutions the relevant one
             % (left or right intersection)
             if prevWP.x < obj.controlPoint.x
                 zpx1 = min(zpx11, zpx12);
             else
                 zpx1 = max(zpx11, zpx12);
             end
-        
+
             if prevWP.y < obj.controlPoint.y
                 zpy1 = min(zpy11, zpy12);
             else
                 zpy1 = max(zpy11, zpy12);
             end
-        
+
             % ZP 2:
             % - angle of incidence of following linear part
             phi2 = atan2(nextWP.y - obj.controlPoint.y, nextWP.x - obj.controlPoint.x);
-        
+
             % - x-coordinates where following linear part intersects with circle
             zpx21 = obj.controlPoint.x - obj.r * cos(phi2);
             zpx22 = obj.controlPoint.x + obj.r * cos(phi2);
             % - y-coordinates where former linear part intersects with circle
             zpy21 = obj.controlPoint.y - obj.r * sin(phi2);
             zpy22 = obj.controlPoint.y + obj.r * sin(phi2);
-        
-            % - decide which of the 2 solutions the relevant one 
+
+            % - decide which of the 2 solutions the relevant one
             % (left or right intersection)
             if nextWP.x < obj.controlPoint.x
                 zpx2 = min(zpx21, zpx22);
             else
                 zpx2 = max(zpx21, zpx22);
             end
-        
+
             if nextWP.y < obj.controlPoint.y
                 zpy2 = min(zpy21, zpy22);
             else
                 zpy2 = max(zpy21, zpy22);
             end
-        
+
             startPoint.x = zpx1;
             startPoint.y = zpy1;
             endPoint.x = zpx2;
             endPoint.y = zpy2;
         end
-        
+
     end
 
-    
+
     methods (Access = protected)
-                
+
         function len = calculateLength(obj)
             len = sum(sqrt(diff(obj.curvePoints.x).^2 + diff(obj.curvePoints.y).^2));
         end
