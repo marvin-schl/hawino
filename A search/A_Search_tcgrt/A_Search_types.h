8c326b2d6780837b8de3fd375c73deb3bc07f821
Leif <leif.johannsen@haw-hamburg.de>
2023-03-25 16:02:54 +0100

    A_search in sps

---
+++ A search/A_Search_tcgrt/A_Search_types.h
@@ -0,0 +1,29 @@
+/*
+ * A_Search_types.h
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "A_Search".
+ *
+ * Model version              : 1.26
+ * Simulink Coder version : 9.6 (R2021b) 14-May-2021
+ * C++ source code generated on : Sat Mar 25 15:22:51 2023
+ *
+ * Target selection: TwinCatGrt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_A_Search_types_h_
+#define RTW_HEADER_A_Search_types_h_
+
+/* Model Code Variants */
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_A_Search_T RT_MODEL_A_Search_T;
+
+#endif                                 /* RTW_HEADER_A_Search_types_h_ */
