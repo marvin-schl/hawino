589464292e86d10c66af9e821e7366d126f70f50
Marvin <marvin.schlageter@haw-hamburg.de>
2023-03-29 18:12:29 +0200

    Stand: Präsentation Bahnregeleung funktioniert weitestgehend, Bahnplanung arbeitet, Ergebnis der Bahnplanung noch optimierbar

--- A search/A_Search_tcgrt/_products/TwinCAT RT (x86)/Release/A_Search/A_SearchVersion.h
+++ A search/A_Search_tcgrt/_products/TwinCAT RT (x86)/Release/A_Search/A_SearchVersion.h
@@ -6,28 +6,28 @@
 
 #define A_Search_Version   0
 #define A_Search_Revision  0
-#define A_Search_Build     3
+#define A_Search_Build     1
 
 #define A_Search_FileVersionMajor    0
 #define A_Search_FileVersionMinor    0
 #define A_Search_FileVersionBuild    0
-#define A_Search_FileVersionRevision 3
-#define A_Search_FileVersionStr      "0.0.0.3"
-#define A_Search_FileVersionRcStr    "0,0,0,3"
+#define A_Search_FileVersionRevision 1
+#define A_Search_FileVersionStr      "0.0.0.1"
+#define A_Search_FileVersionRcStr    "0,0,0,1"
 
 #define A_Search_ProductMajor        0
 #define A_Search_ProductMinor        0
 #define A_Search_ProductBuild        0
-#define A_Search_ProductRevision     3
-#define A_Search_ProductVersionStr   "0.0.0.3"
-#define A_Search_ProductVersionRcStr "0,0,0,3"
+#define A_Search_ProductRevision     1
+#define A_Search_ProductVersionStr   "0.0.0.1"
+#define A_Search_ProductVersionRcStr "0,0,0,1"
 
 #define A_Search_CompanyNameStr      "TE140x Module Vendor"
 #define A_Search_InternalNameStr     "A_Search"
-#define A_Search_LibraryIdNameStr    "TE140x Module Vendor|A_Search|0.0.0.3"
+#define A_Search_LibraryIdNameStr    "TE140x Module Vendor|A_Search|0.0.0.1"
 
 #ifndef RC_INVOKED
-static const GUID VID_A_Search      = { 0x27d616e3, 0x704b, 0xc812, { 0x18, 0x0a, 0xfe, 0x10, 0x74, 0x35, 0x85, 0x03 } };
+static const GUID VID_A_Search      = { 0xad45a022, 0x883f, 0xab99, { 0xe1, 0xaf, 0x90, 0x1d, 0x99, 0xf3, 0x18, 0xff } };
 #endif
 
 #endif // include guard
